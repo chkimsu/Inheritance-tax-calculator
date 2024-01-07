28eb1ce6bdb818b7898d53f28ac6e19df39cdfe8
soobin.choi <chkimsu@naver.com>
2024-01-07 13:58:26 +0900

    2024-01-07

--- src/components/MergeAll.vue
+++ src/components/MergeAll.vue
@@ -6,16 +6,18 @@
     <div class="row">
         <div class="col-md-6">
 
-    <WifeMoney v-on:WifeMoneychildEvent="processCalculateChildEvent" title="배우자 상속 금액 :"></WifeMoney>
+    <WhoInherit v-on:whochildEvent="whopreocesschildevent"></WhoInherit>
+    <WifeMoney v-on:WifeMoneychildEvent="processCalculateChildEvent" v-if="whorecivedata" title="배우자 상속 금액"></WifeMoney>
 
     <div class = "d-flex justify-content-center">
         <CounterCardMerge></CounterCardMerge>
     </div>
     
-    <SamePerson v-on:childEvent="processChildEvent"></SamePerson>
-    <WifeMoney v-on:WifeMoneychildEvent="processCalculateChildEvent" v-if="receivedata" title="동일인 증여 금액 :" ></WifeMoney>
-    <WifeMoney v-on:WifeMoneychildEvent="processCalculateChildEvent" v-for="title in titleList" v-bind:title="title" :key="title"></WifeMoney>
 
+    <WifeMoney v-on:WifeMoneychildEvent="processCalculateChildEvent" v-for="title in titleList.slice(0,2)" v-bind:title="title" :key="title"></WifeMoney>
+    <SamePerson v-on:childEvent="processChildEvent"></SamePerson>
+    <WifeMoney v-on:WifeMoneychildEvent="processCalculateChildEvent" v-if="receivedata" title="동일인 증여 금액" ></WifeMoney>
+    <WifeMoney v-on:WifeMoneychildEvent="processCalculateChildEvent" v-for="title in titleList.slice(2,8)" v-bind:title="title" :key="title"></WifeMoney>
 
 </div>
 
@@ -40,6 +42,7 @@ import CounterCardMerge from '@/components/CounterCardMerge.vue';
 import SamePerson from './SamePerson.vue';
 import HeaderTab from '@/components/HeaderTab.vue'
 import testComp from '@/components/ShowResultPage.vue'
+import WhoInherit from './WhoInherit.vue';
 
 export default {
 
@@ -47,9 +50,10 @@ data(){
  return {
     isTrue : true,
     receivedata : null,
-    titleList : ['상속 재산 :', '금융 상속 재산 :', '채무 금액 :', '감정평가 수수료 :', "장례 비용 :","세대 생략 상속 비용 :","공동주택공제금액 :", "재해손실금액 :" ],
+    titleList : ['상속 재산', '금융 상속 재산', '채무 금액', '감정평가 수수료', "장례 비용","세대 생략 상속 비용","공동주택공제금액", "재해손실금액" ],
     titleObjects: {} ,
-    sum : 0
+    sum : 0,
+    whorecivedata : true
 
 }
 },
@@ -59,8 +63,8 @@ created(){
   // titleList의 각 요소에 대한 객체 생성
     this.titleObjects = this.titleList.reduce((obj, title) => {
     obj[title] = 0; // title을 key로 갖는 빈 문자열 값의 객체 생성
-    obj['동일인 증여 금액 :'] = 0;
-    obj['배우자 상속 금액 :'] = 0;
+    obj['동일인 증여 금액'] = 0;
+    obj['배우자 상속 금액'] = 0;
     return obj;
   }, {});
 
@@ -73,13 +77,18 @@ methods:{
     processChildEvent(data){
         this.receivedata = data
         if(this.receivedata === false){
-            this.titleObjects['동일인 증여 금액 :'] = 0;
-            this.$store.commit('updateDataFromChild', {'title' : '동일인 증여 금액 :' , 'value' :  0}); // Vuex Store로 데이터 전달 및 상태 업데이트
+            this.titleObjects['동일인 증여 금액'] = 0;
+            this.$store.commit('updateDataFromChild', {'title' : '동일인 증여 금액' , 'value' :  0}); // Vuex Store로 데이터 전달 및 상태 업데이트
 
         }
     },
     processCalculateChildEvent(title, data){
         this.titleObjects[title] = data;
+    },
+    whopreocesschildevent(data){
+        this.whorecivedata = data
+        console.log('this.whorecivedata : ', this.whorecivedata)
+
     }
 },
 
@@ -95,7 +104,8 @@ components : {
     CounterCardMerge,
     SamePerson,
     testComp,
-    HeaderTab
+    HeaderTab,
+    WhoInherit
 }
 
 }
